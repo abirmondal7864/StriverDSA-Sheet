5309c4366e91c3994beef7c2c559326a004b7fd8
Abir Mondal <102358238+abirmondal7864@users.noreply.github.com>
2025-09-01 13:57:21 +0530

    Refactor `ValidPalindrome` class: rename method to `isPalindrome` and adjust formatting for clarity.

--- src/basics/recursion/ValidPalindrome.java
+++ src/basics/recursion/ValidPalindrome.java
@@ -1,28 +1,31 @@
 package basics.recursion;
+
+class ValidPalindrome {
 /**
  * Checks if a given string is a valid palindrome.
- *
+ * <p>
  * A valid palindrome ignores spaces, punctuation, and case differences.
  * Only alphanumeric characters are considered for comparison.
  *
  * @param s the input string to check
  * @return true if the string is a palindrome, false otherwise
  */
-public boolean ValidPalindrome(String s) {
-    int left = 0, right = s.length() - 1;
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
 
-    while (left < right) {
-        if (!Character.isLetterOrDigit(s.charAt(left))) {
-            left++;
-        } else if (!Character.isLetterOrDigit(s.charAt(right))) {
-            right--;
-        } else if (Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))) {
-            left++;
-            right--;
-        } else {
-            return false;
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            } else if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            } else if (Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))) {
+                left++;
+                right--;
+            } else {
+                return false;
+            }
         }
-    }
 
-    return true;
+        return true;
+    }
 }
