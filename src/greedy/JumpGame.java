2c5541e4af7a2d01daaa0c8b3543cb224d4478dc
Abir Mondal <102358238+abirmondal7864@users.noreply.github.com>
2026-04-05 00:12:24 +0530

    Add JumpGame class to determine if it's possible to jump to the last index of an array, implementing a greedy approach to track the farthest reachable index.

---
+++ src/greedy/JumpGame.java
@@ -0,0 +1,15 @@
+package greedy;
+
+class JumpGame{
+    public boolean canJump(int[]nums){
+        int maxReach=0;
+        for(int i=0;i<nums.length;i++){
+        // If current index is unreachable
+        if(i>maxReach){return false;}
+
+        // Update the farthest reachable index
+        maxReach=Math.max(maxReach,i+nums[i]);}
+
+        return true;
+    }
+}
